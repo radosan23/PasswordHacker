b582c0a897a712a370d3020ff56c6aedf6fc1fcb
radosan <radoslaw.sanetra@gmail.com>
2023-03-25 22:50:29 +0100

    cracking login and password according to server responses messages serialized to json

--- Password Hacker (Python)/task/hacking/server.py
+++ Password Hacker (Python)/task/hacking/server.py
@@ -1,7 +1,9 @@
+import json
 import socket
 
 
 def set_server():
+    true_data = {'login': 'su', 'password': 'SuP12e'}
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.bind(('127.0.0.1', 9090))
         server.listen()
@@ -11,11 +13,18 @@ def set_server():
                 data = conn.recv(1024).decode()
                 if not data:
                     break
+                data = json.loads(data)
                 print(data)
-                if data == 'qWerTy123':
-                    conn.send('Connection success!'.encode())
+                if data == true_data:
+                    msg = 'Connection success!'
+                elif data['login'] == true_data['login'] and data['password'] \
+                        and true_data['password'].startswith(data['password']):
+                    msg = 'Exception happened during login'
+                elif data['login'] == true_data['login']:
+                    msg = 'Wrong password!'
                 else:
-                    conn.send('wrong!'.encode())
+                    msg = 'Wrong login!'
+                conn.send(json.dumps({'result': msg}).encode())
 
 
 set_server()
